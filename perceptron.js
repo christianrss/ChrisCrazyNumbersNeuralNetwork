535207be9a97525cda65d5801502259b8bc2cb58
Christian Rafael <christianparadix@gmail.com>
2025-10-15 22:02:27 -0300

    Implements perceptron & compute sums

---
+++ perceptron.js
@@ -0,0 +1,35 @@
+const trainInputs = [
+    [2, 7],
+    [3, 6],
+    [1, 1],
+    [1, 2],
+    [2, 1]
+];
+
+const trainLables = [1, 1, 0, 0, 0];
+
+const weights = [0.1, -0.3];
+
+class Perceptron {
+    constructor(learningRate = 0.1) {
+        this.weights = [0.1, -0.3];
+        this.bias = 0.5;
+        this.learningRate = learningRate;
+    }
+
+    train(trainData) {
+        for (let i = 0; i < trainData.length; i++) {
+            let sum = this.bias;
+            let inputs = trainData[i];
+            
+            for (let j = 0; j < inputs.length; j++) {
+                sum += inputs[j] * this.weights[j];
+            }
+
+            console.log(sum);
+        }
+    }
+}
+
+const perceptron = new Perceptron();
+perceptron.train(trainInputs);
