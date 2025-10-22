73e38751b8b39cf25f85948332e71e4dc747c2d3
Christian Rafael <christianparadix@gmail.com>
2025-10-21 22:55:20 -0300

    Read info bytes

--- MnistReader.js
+++ MnistReader.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 
 function readIdxFile(filepath) {
     const data = fs.readFileSync(filepath);
-    
     let offset = 0;
+    const magicNumber = data.readUint32BE(offset);
 
-    const magicNumber = data.readUint32BE(0);
-    console.log(magicNumber);
+
+    offset += 4;
+    const numberOfItems = data.readUint32BE(offset);
+    offset += 4;
+    const rows = data.readUint32BE(offset);
+    offset += 4;
+    const cols = data.readUint32BE(offset);
 }
 
 readIdxFile("./datasets/mnist/train-images.idx3-ubyte");
